8902f9cb607b3baa767bc90f1f4508c0d65f1a2e
David Wolverton <dwolvert@gmail.com>
2020-09-09 16:12:11 -0400

    this is master

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Methods.greet("David Wolverton");
+		System.out.println("I am master.");
 	}
 
 }
